dcb1eb8b895472eeffd604de4b91e705b04b889e
Juanooo07 <jfura@example.com>
2026-06-19 10:56:47 -0500

    fix: remove invalid next.config options

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  typescript: {
-    strictNullChecks: true,
-  },
-  eslint: {
-    dirs: ['src'],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
